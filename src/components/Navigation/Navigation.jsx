726d50aab698d46c78d4782118e4060dbf5752cd
viktoryka <mikisum321@gmail.com>
2023-02-24 21:45:42 +0100

    feat: change category in navigatin with RTK

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,17 +1,35 @@
 import classNames from 'classnames'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeCategory } from '../../store/category/categorySlice'
 import { Layout } from '../Layout/Layout'
 import style from './Navigation.module.css'
 
 export const Navigation = () => {
-  return (
 
+  const { category, activeCategory } = useSelector((state) => state.category)
+  const dispatch = useDispatch()
+
+  return (
     <nav className={style.navigation}>
       <Layout className={style.container}>
         <ul className={style.list}>
-          <li className={style.item}>
-            <button className={classNames(style.button, style.button_burger, style.button_active)}>Бургеры</button>
-          </li>
+          {category.map((item, i) =>
+            <li className={style.item} key={i}>
+              <button
+                className={classNames(
+                  style.button,
+                  activeCategory === i ? style.button_active : '')}
+                style={{ backgroundImage: `url(${item.image})` }}
+                onClick={() => {
+                  dispatch(changeCategory({ indexCategory: i }))
+                }}
+              >
+                {item.rus}
+              </button>
+            </li>
+          )}
+
           <li className={style.item}>
             <button className={classNames(style.button, style.button_snack)}>Закуски</button>
           </li>
