03ff9c145a3ab6687417ea59f7125c42579a0401
viktoryka <mikisum321@gmail.com>
2023-04-04 13:55:29 +0200

    feat:add products in shopping cart

--- src/components/Catalog/CatalogProduct/CatalogProguct.jsx
+++ src/components/Catalog/CatalogProduct/CatalogProguct.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { API_URI } from '../../../const'
+import { addProduct } from '../../../store/order/orderSlice'
 import style from './CatalogProguct.module.css'
 
 export const CatalogProguct = ({ item }) => {
+
+  const dispatch = useDispatch()
+
   return (
     <article className={style.product}>
       <img src={`${API_URI}/${item.image}`} alt={item.title} className={style.image} />
@@ -15,7 +20,15 @@ export const CatalogProguct = ({ item }) => {
 
       <p className={style.weight}>{item.weight}</p>
 
-      <button className={style.add} type="button">Добавить</button>
+      <button
+        className={style.add}
+        type="button"
+        onClick={() => {
+          dispatch(addProduct({ id: item.id }))
+        }}
+      >
+        Добавить
+      </button>
     </article>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from "./category/categorySlice";
+import orderReducer, { localStorageMiddleware } from './order/orderSlice';
 import productReducer from './product/productSlice';
 
 export const store = configureStore({
   reducer: {
     category: categoryReducer,
-    product: productReducer
-  }
+    product: productReducer,
+    order: orderReducer
+  },
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(localStorageMiddleware)
+
 })
